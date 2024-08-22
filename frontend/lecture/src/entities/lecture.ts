930c2e22ef2d11b25917943d079d1c21b998aca7
woohm402 <woohm404@gmail.com>
2024-08-22 22:19:19 +0900

    rebuild with tailwind&shadcn

--- frontend/lecture/src/entities/lecture.ts
+++
@@ -1,5 +0,0 @@
-export const LECTURE_INDEXES = [0, 1, 2, 3, 4] as const;
-
-export type Lecture = {
-  lectureIndex: (typeof LECTURE_INDEXES)[number];
-};
--- frontend/lecture/vite.config.ts
+++ frontend/lecture/vite.config.ts
@@ -1,50 +1,9 @@
 import react from '@vitejs/plugin-react-swc';
-import fs from 'fs/promises';
-import { defineConfig, Plugin } from 'vite';
-
-const lecturePlugin = (): Plugin => {
-  const virtualModuleId = 'virtual:lecture-contents';
-
-  return {
-    name: 'lecture-plugin',
-    resolveId(id) {
-      if (id === virtualModuleId) return virtualModuleId;
-    },
-    async load(id) {
-      if (id === virtualModuleId) {
-        const folders = await fs.readdir('./contents').catch(() => []);
-
-        const foldersWithFiles = await Promise.all(
-          folders.map(async (folder) => {
-            const files = (
-              await fs.readdir(`./contents/${folder}`).catch(() => [])
-            )
-              .filter((file) => file.endsWith('.md'))
-              .sort();
-
-            const markdowns = await Promise.all(
-              files.map(async (file) => {
-                const markdown = await fs.readFile(
-                  `./contents/${folder}/${file}`,
-                  'utf-8',
-                );
-                return markdown;
-              }),
-            );
-
-            return { folder, markdowns };
-          }),
-        );
-
-        return `export const contents = ${JSON.stringify(foldersWithFiles)};`;
-      }
-    },
-  };
-};
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), lecturePlugin()],
+  plugins: [react()],
   server: { port: 3025 },
   resolve: { alias: { '@': '/src' } },
   assetsInclude: ['**/*.md'],
