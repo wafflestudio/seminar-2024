0e19db6ae520862bbfb3d22e81c9e16051a61590
Hyunmin Woo (Hanul) <woohm404@gmail.com>
2024-08-30 18:58:31 +0900

    ✨ 날짜 조정 (#16)

--- frontend/lecture/src/App.tsx
+++ frontend/lecture/src/App.tsx
@@ -1,9 +1,11 @@
 import { CalendarIcon } from '@radix-ui/react-icons';
-import { ReactNode } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { ReactNode, useState } from 'react';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 
+import { InlineCode } from '@/components/InlineCode';
 import { ThemeToggle } from '@/components/ThemeToggle';
 import { Separator } from '@/designsystem/ui/separator';
+import { Tabs, TabsList, TabsTrigger } from '@/designsystem/ui/tabs';
 import { useToggleTheme } from '@/main';
 import { EnvironmentAssignment } from '@/pages/Environment/assignment';
 import { Environment } from '@/pages/Environment/lecture';
@@ -30,7 +32,7 @@ const pages: (
       title: string;
       path: string;
       element: ReactNode;
-      due: Date;
+      schedule: [Date, Date];
     }
 )[] = [
   { type: 'index', path: '/', element: <Home /> },
@@ -38,29 +40,47 @@ const pages: (
     type: 'lecture',
     title: 'OT',
     description: '세미나 목표, 방식, 과제, 평가',
-    date: new Date('2024-09-11'),
+    date: new Date('2024-09-04'),
     path: '/ot',
     element: <OT />,
   },
   {
     type: 'assignment',
-    title: 'OT 과제',
-    due: new Date('2024-09-25'),
-    path: '/como-1',
+    title: '세미나 0 코모',
+    schedule: [new Date('2024-09-04'), new Date('2024-09-11')],
+    path: '/como-0',
+    element: (
+      <h2 className="flex h-full flex-col items-center justify-center">
+        <div>
+          조별 DM방에서 날짜 잡고 코모 진행 후{' '}
+          <InlineCode code="#frontend-잡담" /> 채널에 인증샷 업로드
+        </div>
+        <br />
+        <b className="text-base text-slate-500">
+          사진에 얼굴은 필수가 아니지만 노트북은 필수입니다
+        </b>
+      </h2>
+    ),
+  },
+  {
+    type: 'assignment',
+    title: '리액트 튜토리얼',
+    schedule: [new Date('2024-09-04'), new Date('2024-09-25')],
+    path: '/vlpt-tutorial',
     element: <OTAssignment />,
   },
   {
     type: 'lecture',
     title: '개발환경 세팅',
     description: 'IDE, eslint, prettier, ci',
-    date: new Date('2024-09-11'),
+    date: new Date('2024-09-04'),
     path: '/environment',
     element: <Environment />,
   },
   {
     type: 'assignment',
     title: '개발 환경 설정',
-    due: new Date('2024-09-25'),
+    schedule: [new Date('2024-09-04'), new Date('2024-09-11')],
     path: '/setup-environment',
     element: <EnvironmentAssignment />,
   },
@@ -68,7 +88,7 @@ const pages: (
     type: 'lecture',
     title: '웹과 언어',
     description: '웹의 역사, html, css, js의 역할과 동작, typescript의 필요성',
-    date: new Date('2024-09-25'),
+    date: new Date('2024-09-11'),
     path: '/web-html-css-js-ts',
     element: <WebBasic />,
   },
@@ -76,14 +96,14 @@ const pages: (
     type: 'lecture',
     title: '리액트 맛보기',
     description: 'typescript 기초, React 의 JSX / state / props',
-    date: new Date('2024-09-25'),
+    date: new Date('2024-09-11'),
     path: '/react-basic',
     element: <ReactBasic />,
   },
   {
     type: 'assignment',
     title: '리액트 기초 과제',
-    due: new Date('2024-10-09'),
+    schedule: [new Date('2024-09-25'), new Date('2024-10-09')],
     path: '/react-basic-assignment',
     element: <ReactBasicAssignment />,
   },
@@ -91,7 +111,7 @@ const pages: (
     type: 'lecture',
     title: '리액트',
     description: '리액트의 철학, 상태 설계, Context, Hook',
-    date: new Date('2024-10-02'),
+    date: new Date('2024-09-25'),
     path: '/react',
     element: <div>TBD</div>,
   },
@@ -99,15 +119,15 @@ const pages: (
     type: 'lecture',
     title: '스타일링',
     description: 'inline style, css-in-js, css modules, atomic css',
-    date: new Date('2024-10-16'),
+    date: new Date('2024-10-02'),
     path: '/styling',
     element: <div>TBD</div>,
   },
   {
     type: 'lecture',
-    title: '클린 코드',
+    title: '클린 코드와 아키텍처',
     description: 'DRY, SRP, DI',
-    date: new Date('2024-10-16'),
+    date: new Date('2024-10-02'),
     path: '/architecture',
     element: <div>TBD</div>,
   },
@@ -115,23 +135,31 @@ const pages: (
     type: 'lecture',
     title: '비동기',
     description: 'callback, Promise, event loop',
-    date: new Date('2024-11-06'),
+    date: new Date('2024-10-16'),
     path: '/async',
     element: <div>TBD</div>,
   },
   {
     type: 'lecture',
-    title: 'SSR 프레임워크',
+    title: '성능과 SSR',
     description: 'Next.js',
-    date: new Date('2024-11-20'),
+    date: new Date('2024-11-06'),
     path: '/SSR',
+    element: <div>TBD</div>,
+  },
+  {
+    type: 'lecture',
+    title: '메타 프레임워크',
+    description: 'Next.js',
+    date: new Date('2024-11-06'),
+    path: '/meta-framework',
     element: <div>TBD</div>,
   },
   {
     type: 'lecture',
     title: '프론트엔드 인프라',
     description: '',
-    date: new Date('2024-11-20'),
+    date: new Date('2024-11-13'),
     path: '/infrastructure',
     element: <div>TBD</div>,
   },
@@ -139,7 +167,7 @@ const pages: (
     type: 'lecture',
     title: '마무리',
     description: '',
-    date: new Date('2024-11-20'),
+    date: new Date('2024-11-13'),
     path: '/end',
     element: <div>TBD</div>,
   },
@@ -163,36 +191,59 @@ export const App = () => {
 
 const Sidebar = () => {
   const toggleTheme = useToggleTheme();
+  const { tab, onChangeTab } = useTab();
+  const currentPath = useLocation().pathname;
 
   return (
     <div className="flex w-52 flex-col py-4 bg-blend-darken">
       <Link to="/">
         <h1 className="text-center text-3xl font-bold">Lecture</h1>
       </Link>
-      <nav className="mt-4 flex-1 overflow-y-scroll px-4 text-xl">
-        <ul className="flex flex-col pt-4">
+      <Tabs
+        className="mx-4 mt-8"
+        value={tab}
+        onValueChange={(e) => {
+          onChangeTab(e as 'lecture' | 'assignment');
+        }}
+      >
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="lecture">강의</TabsTrigger>
+          <TabsTrigger value="assignment">과제</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <nav className="flex-1 overflow-y-scroll px-4 text-xl">
+        <ul className="mt-4 flex flex-col gap-4">
           {pages.flatMap((page) =>
-            page.type === 'index' ? (
-              []
-            ) : page.type === 'assignment' ? (
-              <li
-                key={page.path}
-                className="ml-2 rounded-sm px-4 py-2 transition-colors hover:bg-slate-200 dark:hover:bg-slate-800"
-              >
-                <Link to={page.path} className="flex flex-col gap-1">
-                  <h3 className="text-base">[과제] {page.title}</h3>
+            tab === 'assignment' && page.type === 'assignment' ? (
+              <li key={page.path}>
+                <Link
+                  to={page.path}
+                  className={`flex flex-col gap-1 rounded-sm px-4 py-2 transition-colors hover:bg-slate-200 dark:hover:bg-slate-800 ${
+                    currentPath === page.path
+                      ? 'bg-blue-100 font-semibold dark:bg-blue-900'
+                      : ''
+                  }`}
+                >
+                  <h3 className="text-base">{page.title}</h3>
                   <p className="flex items-center gap-2 text-xs text-slate-500">
-                    <CalendarIcon /> ~
-                    {formatDate(page.due, ({ MM, DD }) => `${MM}월 ${DD}일`)}
+                    {page.schedule
+                      .map((s) =>
+                        formatDate(s, ({ MM, DD }) => `${MM}월 ${DD}일`),
+                      )
+                      .join(' ~ ')}
                   </p>
                 </Link>
               </li>
-            ) : (
-              <li
-                key={page.path}
-                className="mt-4 rounded-sm px-4 py-2 transition-colors hover:bg-slate-200 dark:hover:bg-slate-800"
-              >
-                <Link to={page.path} className="flex flex-col gap-1">
+            ) : tab === 'lecture' && page.type === 'lecture' ? (
+              <li key={page.path}>
+                <Link
+                  to={page.path}
+                  className={`flex flex-col gap-1 rounded-sm px-4 py-2 transition-colors hover:bg-slate-200 dark:hover:bg-slate-800 ${
+                    currentPath === page.path
+                      ? 'bg-blue-100 font-semibold dark:bg-blue-900'
+                      : ''
+                  }`}
+                >
                   <h3>{page.title}</h3>
                   <p className="text-xs text-slate-400">{page.description}</p>
                   <p className="flex items-center gap-2 text-xs text-slate-500">
@@ -201,6 +252,8 @@ const Sidebar = () => {
                   </p>
                 </Link>
               </li>
+            ) : (
+              []
             ),
           )}
         </ul>
@@ -210,4 +263,17 @@ const Sidebar = () => {
       </div>
     </div>
   );
+};
+
+const useTab = () => {
+  const [tab, setTab] = useState<'lecture' | 'assignment'>(
+    localStorage.getItem('tab') === 'assignment' ? 'assignment' : 'lecture',
+  );
+
+  const onChangeTab = (newTab: typeof tab) => {
+    setTab(newTab);
+    localStorage.setItem('tab', newTab);
+  };
+
+  return { tab, onChangeTab };
 };
--- frontend/lecture/src/pages/OT/assignment.tsx
+++ frontend/lecture/src/pages/OT/assignment.tsx
@@ -3,24 +3,21 @@ import { ExternalLink } from '@/components/ExternalLink';
 export const OTAssignment = () => {
   return (
     <div className="flex h-full items-center justify-center">
-      <ul className="flex list-disc flex-col gap-4 pl-10">
-        <li>조원 확인 및 채널/DM방 개설 (~오늘)</li>
+      <ul className="flex flex-col gap-4 pl-10">
         <li>
-          다음 세미나 전까지 코모 1회 진행 후 #frontend-잡담 채널에 인증샷
+          벨로퍼트와 함께 하는 모던 리액트 1.3~1.16 까지 튜토리얼 진행하면서
+          개념 정리하고, 정리한 내용 마크다운 형태로 #frontend-잡담 채널에
           업로드
           <br />
+          <ExternalLink href="https://react.vlpt.us/" />
+          <br />
           <b className="text-base text-slate-500">
-            설입이면 세상과 연애하기, 할리스, 커피어반, 커피빈, 관정 스터디룸,
-            토즈모임센터 등
+            이것도 이제 조금 옛날 자료라 틀린 내용도 좀 있긴 합니다
           </b>
           <br />
-          <b className="text-base text-slate-500">얼굴은 안 나와도 됩니다.</b>
-        </li>
-        <li>
-          벨로퍼트와 함께 하는 모던 리액트 1-16 까지 튜토리얼 진행하면서 개념
-          정리하고, 정리한 내용 마크다운 형태로 #frontend-잡담 채널에 업로드
-          <br />
-          <ExternalLink href="https://react.vlpt.us/" />
+          <b className="text-base text-slate-500">
+            만약 이미 리액트를 할 줄 알아서 의미없게 느껴진다면 DM 주세요
+          </b>
         </li>
       </ul>
     </div>
