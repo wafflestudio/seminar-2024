17251e06d33f28bdeb8ff2c95ff0357b1be81ee4
woohm402 <woohm404@gmail.com>
2024-09-12 12:00:52 +0900

    fix lint

--- frontend/lecture/eslint.config.js
+++ frontend/lecture/eslint.config.js
@@ -1,7 +1,7 @@
 import react from '@woohm402/eslint-config-react';
 
 export default [
-  { ignores: ['eslint.config.js', '.yarn', 'postcss.config.js'] },
+  { ignores: ['eslint.config.js', '.yarn', 'postcss.config.js', 'dist'] },
   ...react({
     tsconfigRootDir: import.meta.dirname,
   }),
